9a249dabf317382c56c1bf5298d9adc35e26eb6d
Sandip77-hub <72593171+Sandip77-hub@users.noreply.github.com>
2021-05-04 17:52:46 +0545

    Add files via upload
    To check either male or person and to  find the sum of two number.

---
+++ male or person.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int gender,M;
+	cout <<"Enter 0 or 1";
+	cin >> gender;
+	if(gender==1)
+	{
+		cout<<"Your gender is male."<< gender << M;
+	}
+cout<<"You are a person.";
+
+	
+return 0;
+	
+	
+}
